df13c4d5433a267fe8c55f61148fd7e4daf76424
vy-shoe <vadimulam6@gatech.edu>
2022-01-04 21:45:49 -0500

    Using observer pattern for state management and making call to API .onAppear

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -8,13 +8,29 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @ObservedObject var networkManager = NetworkManager() //subscribing to updates in networkManager networkManager
+    
+    /* 1. View Loads up with a nav view and nav title
+       2. As soon as it appears, we trigger fetch data method in networkManager
+            a. in NM, we decode if there are errors, we catch and print
+            b. if not, we save results into posts array
+            c. since post property is a Published variable, the networkManager is subscribed and will update UI
+     */
     var body: some View {
         NavigationView {
             //for every post in post array, make a Test item using it's title
-            List(posts) { post in
-                Text(post.title)
-            }
-        }.navigationTitle("hackernews")
+            List(networkManager.posts) { post in
+                HStack {
+                    Text(String(post.points))
+                    Text(post.title)
+                }
+            }// whenever post changes, List issues a rebuild of what's inside
+            .navigationTitle("hackernews")
+        }
+        .onAppear {
+            self.networkManager.fetchData()
+        }
     }
 }
 
@@ -24,14 +40,8 @@ struct ContentView_Previews: PreviewProvider {
     }
 }
 
-//allows list to recognize the order by id
-struct Post: Identifiable {
-    let id: String
-    let title: String
-}
-
-let posts = [
-    Post(id: "1", title: "Hello"),
-    Post(id: "1", title: "Bonjour"),
-    Post(id: "1", title: "Hola")
-]
+//let posts = [
+//    Post(id: "1", title: "Hello"),
+//    Post(id: "1", title: "Bonjour"),
+//    Post(id: "1", title: "Hola")
+//]
--- Shared/Models/NetworkManager.swift
+++ Shared/Models/NetworkManager.swift
@@ -6,3 +6,36 @@
 //
 
 import Foundation
+
+//ObservableObject protocol broadcasts its properties to any interested parties
+
+class NetworkManager: ObservableObject {
+    @Published var posts = [Post]() //if you "subscribe to the property called post, you'll hear from it everytime it changes
+    
+    func fetchData() {
+        if let url = URL(string: "http://hn.algolia.com/api/v1/search?tags=front_page") {
+            let session = URLSession(configuration: .default)
+            let task = session.dataTask(with: url) { (data, response, error) in
+                if error == nil { //if there is no error
+                    let decoder = JSONDecoder()
+                    if let safeData = data {
+                        do {
+                            let results = try decoder.decode(Results.self, from: safeData)
+                            
+                            //Since posts is a property other objects are listening to, so the update must happen on the main thread and must not happen in the background or else it wont update in a timely fashion
+                            DispatchQueue.main.async {
+                                self.posts = results.hits
+                            }
+                            
+                        } catch {
+                            print(error)
+                        }
+                    }
+                    
+                }
+            }
+            task.resume() //starts networking task and
+        }
+        // now we can address results and get the data from the NetworkManager into the ContentView- use observer design pattern
+    }
+}
